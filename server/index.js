0770a184959286cf86b89321069229e2759a4b7a
tarinagarwal <theultimatemania64@gmail.com>
2025-08-21 20:20:32 +0530

    prod fixes #3

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "alienvault.vercel.app"],
+    origin: ["http://localhost:5173", "https://alienvault.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "alienvault.vercel.app"],
+    origin: ["http://localhost:5173", "https://alienvault.vercel.app"],
     credentials: true,
   })
 );
