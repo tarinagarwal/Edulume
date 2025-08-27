90822e3e9e37c4640b60d5822bfed49672f71349
SpandyX007 <spandanar1234@gmail.com>
2025-08-28 00:05:06 +0530

    pdf chat (pinecone & ML.pdf)

---
+++ AlienVault_backend/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, Query as FastAPIQuery, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import RAG_app
+
+class Query(BaseModel):
+    user_query: str
+
+class Response(BaseModel):
+    rag_response: str
+
+app = FastAPI()
+origins = ['http://localhost:1000']  # add the local/deployment frontend URL here
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# Global variable for chat history per session
+chat_history = {}
+gemini_chat_history = {}
+
+# fetching response from RAG
+def rag_response(query, session_chat_history):
+    rag_resp = RAG_app.main(query=query, chat_history=session_chat_history)
+    return rag_resp
+
+# Fetching user request using POST request
+@app.post("/query", response_model=Response)
+async def fetch_query(query: Query, session_id: str = FastAPIQuery(...)):
+    if session_id not in chat_history:
+        chat_history[session_id] = []
+    if session_id not in gemini_chat_history:
+        gemini_chat_history[session_id] = []
+    rag_resp = RAG_app.main(query.user_query, gemini_chat_history[session_id])
+    chat_history[session_id].append({"user_query": query.user_query, "rag_response": rag_resp})
+    if rag_resp is not None:
+        return Response(rag_response=rag_resp)
+    else:
+        return Response(rag_response="")
+
+# Providing the response using get request
+@app.get("/history", response_model=list)
+async def get_history(session_id: str = FastAPIQuery(...)):
+    if session_id not in chat_history:
+        raise HTTPException(status_code=404, detail="No history found for this session.")
+    return chat_history[session_id]
